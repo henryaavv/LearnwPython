5f64bcc5f302ba179fae23736535bf078539a6e1
Henry Argandoña <139931739+henryaavv@users.noreply.github.com>
2025-01-22 11:54:46 -0500

    agregue añitos

---
+++ hola.py
@@ -0,0 +1,13 @@
+print("Hola mundo")
+def suma(a,b):
+    return a + b
+
+if 1>5:
+    print("8 es mayor que 5")
+print("10 es mayor que 5")
+        
+
+nombre="Henry"
+edad=25
+print(f"hola {nombre} tienes {edad} años")
+print("hola {nombre} tienes {edad} años")
