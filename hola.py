19bd1f812de243d5710ba54e10e868c40390116f
Henry Argandoña <139931739+henryaavv@users.noreply.github.com>
2025-01-23 15:41:57 -0500

    luego de que hayan guardado el primer commit

--- hola.py
+++ hola.py
@@ -6,8 +6,8 @@ if 1>5:
     print("8 es mayor que 5")
 print("10 es mayor que 5")
         
-
 nombre="Henry"
 edad=25
 print(f"hola {nombre} tienes {edad} años")
-print("hola {nombre} tienes {edad} años")
+
+print("hola {nombre} tienes  {edad} añitos")
